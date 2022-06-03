c09e12fb88fde68d215c4be74b47ec7b4f9567ea
lupinx2 <78879397+lupinx2@users.noreply.github.com>
2022-06-03 08:00:26 -0700

    APIgetAnimeList now fills list of cover image URLs

--- malptw_rand.py
+++ malptw_rand.py
@@ -8,31 +8,30 @@
 #
 # PySimpleGUI:
 #   Add xml selection.
-#   Add randomize button function.
-#   Add radial button functionality
+#   Add randomize button XML function.
 #   Improve output formatting, aspect.
 #       Add cover art.
 #       Add link to MAL page/streaming.
 #
-# Make it so that APIgetAnimeList is only called if the -username- changes.
-#
-# Use the API from the GUI.
-#   Add save button functionality.
+# Use the API key from the GUI.
+#   Add save to config button functionality.
 # 
 # rename variables to be less confusing.
 #
 # Handle all errors in the GUI with user friendly messages.
 #
-# Remove console output
-#   make all the print calls conditional to a DEBUG bool?
+# narrow scope of imports to only what is needed.
+#
+# Remove console output before pull request.
 
-from argparse import ArgumentParser
-from distutils.log import error
-from msilib.schema import CheckBox
+#from argparse import ArgumentParser
+#from distutils.log import error
+#from msilib.schema import CheckBox
 import xml.etree.ElementTree as ET
 import PySimpleGUI as Gooey
 from time import sleep
 from cmd import PROMPT
+from PIL import Image
 import requests
 import config
 import random
@@ -42,8 +41,9 @@ import json
 
 if __name__ == '__main__':
 
-    ptw_list = list()
-    ptw_list_id = list()
+    list_titles = list()
+    list_id = list()
+    list_coverImg = list()
     no_movies = False
     only_movies = False
     prevAPIcall = ""
@@ -61,7 +61,7 @@ if __name__ == '__main__':
                 '/animelist?fields=list_status,media_type&status=plan_to_watch&limit=1000'  # 1000 is the max allowed by MAL.
             headers = {'X-MAL-CLIENT-ID': config.API_key}
             sleep(0.7)  # Sleep to prevent rate limiting.
-            response = requests.get(url, headers=headers)
+            response = requests.get(url, headers=headers)            
             if response.status_code != 200:
                 raise Exception("API call error")
             responseBytes = (response.content)
@@ -85,25 +85,37 @@ if __name__ == '__main__':
                             for result in gen_dict_extract(listItem, key):
                                 yield result
         for item in gen_dict_extract(outputDict, 'node'):
-            ptw_list.append(item['title'] + " [" + item['media_type'] + "]")
-            ptw_list_id.append(item['id'])
+            list_titles.append(item['title'] + " [" + item['media_type'] + "]")
+            list_id.append(item['id'])
+            list_coverImg.append(item['main_picture']['medium'])
             if (no_movies == True and item['media_type'] == "movie"):
-                ptw_list.pop()
-                ptw_list_id.pop()
+                list_titles.pop()
+                list_id.pop()
+                list_coverImg.pop()
             if (only_movies == True and not item['media_type'] == "movie"):
-                ptw_list.pop()
-                ptw_list_id.pop()
+                list_titles.pop()
+                list_id.pop()
+                list_coverImg.pop()
         prevAPIcall = user_name
 
     # Returns a tuple of the anime title and a link to the anime's page on MAL.
     def GetRandomAnime():
         try:
-            rand_index = random.randint(0, len(ptw_list)-1)
-            return "{}".format(ptw_list[rand_index]), ('https://myanimelist.net/anime/' +
-                                                       str(ptw_list_id[rand_index]))
+            rand_index = random.randint(0, len(list_titles)-1)
+            return "{}".format(list_titles[rand_index]), ('https://myanimelist.net/anime/' +
+                                                       str(list_id[rand_index]))
         except:
             print ("Error: Failed to get anime data from list object.\n")
-            return "Error: Failed to reach anime list.", "Error: Failed to reach anime list."
+            return "Error: Failed to get anime from PTW list.", "https://myanimelist.net/anime/20"
+
+    # def GetCoverArt(rand_index):
+    #     try:
+    #         url = "list_coverImg[rand_index]"
+    #         response = requests.get(url, stream=True)
+    #         response.raw.decode_content = True
+    #         img = 
+    #     except: 
+    #        print("hi")
 # ------------------------------------------------------------------------------
 # GUI
 # ------------------------------------------------------------------------------
@@ -186,13 +198,13 @@ if __name__ == '__main__':
     for anime in tree_root.findall("anime"):
         # where my_status == PTW...
         if anime.find("my_status").text == "Plan to Watch":
-            ptw_list.append(anime.find("series_title").text +  # append the series_title to ptw_list...
+            list_titles.append(anime.find("series_title").text +  # append the series_title to ptw_list...
                             " [" + anime.find("series_type").text + "]")  # also include its series_type...
             # then, append its series_animedb_id to ptw_list_id.
-            ptw_list_id.append(anime.find("series_animedb_id").text)
+            list_id.append(anime.find("series_animedb_id").text)
             if (anime.find("series_type").text == "Movie") and (no_movies == "y"):
-                ptw_list.pop()
-                ptw_list_id.pop()
+                list_titles.pop()
+                list_id.pop()
             if (anime.find("series_type").text != "Movie") and (only_movies == "y"):
-                ptw_list.pop()
-                ptw_list_id.pop()
+                list_titles.pop()
+                list_id.pop()
